edf761dcecde51d63b893a6a9e2bceb5ca8623cc
Matt Holmes <mattholmes@live.com>
2015-11-08 20:39:48 -0600

    update

--- applications/Library_Samples/static/js/invite_tool.js
+++ applications/Library_Samples/static/js/invite_tool.js
@@ -1,59 +1,67 @@
+function InviteTool(){};
 function Account(){};
 function Invitee(){};
 
 Account.prototype.collection = function() {
-	 return $.get( "invite_tool/getAccountCollection", function( data ) {});
-}
-
-Account.prototype.item = function(id, name){
-	var self = this;
-	self.id = id;
-	self.name = name;
+	return $.get( "invite_tool/getAccountCollection", function( data ) {});
 }
 
 Invitee.prototype.collection = function(account_id) {
-	 return $.get( "invite_tool/getInviteeCollection/" + account_id, function( data ) {});
+	return $.get( "invite_tool/getInviteeCollection/" + account_id, function( data ) {});
+}
+
+Account.prototype.item = function(id, name, children){
+	this.id = id;
+	this.name = name;
+	this.children = ko.observableArray(children);
 }
 
 Invitee.prototype.item = function(id, first_name, last_name, email) {
-	var self = this;
-	self.first_name = first_name;
-	self.last_name = last_name;
-	self.email = email;
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.email = email;
 }
 
-Account.prototype.item = function(id, name){
-	var self = this;
-	self.id = id;
-	self.name = name;
+Account.prototype.childrenArray = function(data){
+	var children = [];
+	var invitee = new Invitee;
+	_.each(data, function(inviteeModel){
+		children.push(
+		new invitee.item(
+				inviteeModel.id,
+				inviteeModel.first_name,
+				inviteeModel.last_name,
+				inviteeModel.email
+			)
+		);
+	});
+	return children;
 }
 
-function InviteToolViewModel(){
+InviteTool.prototype.viewModel = function(){
     var self = this;
     var account = new Account;
-	var invitee = new Invitee;
-	self.inviteeArray = ko.observableArray([]);
-    self.accountsArray = [];
-
+   	var invitee = new Invitee;
+	self.accountsArray = ko.observableArray([]);
+	
 	accountData = account.collection();
     accountData.done(function(data){
     	_.each(data, function(accountModel){
 			inviteeData = invitee.collection(accountModel.id);
 			inviteeData.done(function(invData){
-				_.each(invData, function(inviteeModel){
-					console.log(inviteeModel.first_name);
-					self.inviteeArray.push(new invitee.item(inviteeModel.id,inviteeModel.first_name,inviteeModel.last_name,inviteeModel.email));
-				});
-			});
-			self.accountsArray.push(new account.item(accountModel.id, accountModel.name));			
+				self.accountsArray.push(
+					new account.item(
+						accountModel.id, 
+						accountModel.name, 
+						account.childrenArray(invData)
+					)
+				);	
+			});		
 		});
-    });
-	console.log(self.inviteeArray);
+    });  
 }
 
-function InviteeViewModel(){
+inviteTool = new InviteTool;
+ko.applyBindings(new inviteTool.viewModel);
 
 
-}
-
-ko.applyBindings(new InviteToolViewModel());
