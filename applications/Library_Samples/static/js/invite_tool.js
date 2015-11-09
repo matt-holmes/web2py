afa16424f8ca52e2da8c730c27596562c7d5119e
Matt Holmes <mattholmes@live.com>
2015-11-08 18:16:46 -0600

    updates

--- applications/Library_Samples/static/js/invite_tool.js
+++ applications/Library_Samples/static/js/invite_tool.js
@@ -12,7 +12,14 @@ Account.prototype.item = function(id, name){
 }
 
 Invitee.prototype.collection = function(account_id) {
-	 return $.get( "invite_tool/getAccountCollection", function( data ) {});
+	 return $.get( "invite_tool/getInviteeCollection/" + account_id, function( data ) {});
+}
+
+Invitee.prototype.item = function(id, first_name, last_name, email) {
+	var self = this;
+	self.first_name = first_name;
+	self.last_name = last_name;
+	self.email = email;
 }
 
 Account.prototype.item = function(id, name){
@@ -24,14 +31,29 @@ Account.prototype.item = function(id, name){
 function InviteToolViewModel(){
     var self = this;
     var account = new Account;
-    self.accountsArray = ko.observableArray([]);
+	var invitee = new Invitee;
+	self.inviteeArray = ko.observableArray([]);
+    self.accountsArray = [];
 
-	jqxhr = account.collection();
-    jqxhr.done(function(data){
-    	_.each(data, function(model){
-			self.accountsArray.push(new account.item(model.id, model.name));
+	accountData = account.collection();
+    accountData.done(function(data){
+    	_.each(data, function(accountModel){
+			inviteeData = invitee.collection(accountModel.id);
+			inviteeData.done(function(invData){
+				_.each(invData, function(inviteeModel){
+					console.log(inviteeModel.first_name);
+					self.inviteeArray.push(new invitee.item(inviteeModel.id,inviteeModel.first_name,inviteeModel.last_name,inviteeModel.email));
+				});
+			});
+			self.accountsArray.push(new account.item(accountModel.id, accountModel.name));			
 		});
     });
+	console.log(self.inviteeArray);
+}
+
+function InviteeViewModel(){
+
+
 }
 
 ko.applyBindings(new InviteToolViewModel());
